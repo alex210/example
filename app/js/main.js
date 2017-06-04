cf1ed934b9382775b7951c31b552c7016b83da75
USER <sanya.groza@yandex.ua>
2017-06-04 23:42:20 +0300

    ex

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	$('#my-menu').mmenu();
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('close:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+
+});
